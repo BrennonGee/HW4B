1fd8965e54658a5ae7f6907c96d4fe6962d68320
GeeMan96 <brennon.gee@gmail.com>
2017-10-08 19:16:48 -0400

    Outline for a binary search tree for HW #4

---
+++ BinarySearchTree.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package binarysearchtree;
+
+/**
+ *
+ * @author brennongee
+ */
+public class BinarySearchTree {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public void Insert(){
+        
+    }
+    
+    public void Delete(){
+        
+    }
+    
+    public void Min(){
+        
+    }
+    
+    public void Max(){
+        
+    }
+    
+    public void Successor(){
+        
+    }
+    
+    public void Predecessor(){
+        
+    }
+    
+    public void Print(){
+        
+    }
+    
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+HW-4B 
+
+This includes the implimentation for a Binary search tree as well as the red black tree built on top of the BST
